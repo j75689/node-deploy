b09a39655e10824171c0be0c2364d2803f0a2157
Roshan <luoshen1997@gmail.com>
2023-11-10 18:07:29 +0800

    update script

--- start_cluster.sh
+++ start_cluster.sh
@@ -13,12 +13,20 @@ declare -A ips2ids
 ips2ids["172.22.42.13"]="i-0d2b8632af953d0f6"
 ips2ids["172.22.42.94"]="i-001b988ca374e66f1"
 ips2ids["172.22.43.86"]="i-0d36ebf557138f8e5"
-ips="172.22.42.13 172.22.42.94 172.22.43.86"
+validator_ips="172.22.42.13 172.22.42.94 172.22.43.86"
 
 rm -rf /mnt/efs/bsc-qa/bc-fusion/clusterNetwork
 cp -r /server/roshan/bc-fusion-test/clusterNetwork /mnt/efs/bsc-qa/bc-fusion
 cp /server/roshan/bc-fusion-test/start_geth.sh /mnt/efs/bsc-qa/bc-fusion/clusterNetwork/.
 cp /server/roshan/bc-fusion-test/stop_geth.sh /mnt/efs/bsc-qa/bc-fusion/clusterNetwork/.
+
+component=$1
+if [[ "${component}"x == "validator"x ]]; then
+    ips=(${validator_ips})
+else
+    echo "no process "
+    exit
+fi
 
 for ((i = 0; i < ${#ips[@]}; i++)); do
     dst_id=${ips2ids[${ips[i]}]}
