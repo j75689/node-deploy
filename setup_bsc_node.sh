40507ffab2b1a19b05bb8f8c0d067271d2fed4bf
j75689 <j75689@gmail.com>
2023-11-30 00:39:52 +0800

    fix: bsc setup scripts

--- setup_bsc_node.sh
+++ setup_bsc_node.sh
@@ -7,8 +7,9 @@ workspace=${basedir}
 source ${workspace}/.env
 source ${workspace}/utils.sh
 size=$((${BSC_CLUSTER_SIZE}))
-standalone=true
-initIp="172.22.42.13,172.22.42.94,172.22.43.86"
+standalone=false
+bsc_node_ips=(${BSC_NODE_IP})
+nodeurl=${BC_NODE_URL}
 
 # need a clean bc without stakings
 function register_validator() {
@@ -60,127 +61,107 @@ function register_validator() {
     done
 }
 
-function create_validator() {
-    rm -rf ${workspace}/clusterNetwork
-
-    for ((i = 0; i < ${size}; i++)); do
-        mkdir -p ${workspace}/clusterNetwork/validator${i}
-        echo "${KEYPASS}" >${workspace}/clusterNetwork/password.txt
-
-        cons_addr=$(${workspace}/bin/geth account new --datadir ${workspace}/clusterNetwork/validator${i} --password ${workspace}/clusterNetwork/password.txt | grep "Public address of the key:" | awk -F"   " '{print $2}')
-        fee_addr=$(${workspace}/bin/geth account new --datadir ${workspace}/clusterNetwork/validator${i}_fee --password ${workspace}/clusterNetwork/password.txt | grep "Public address of the key:" | awk -F"   " '{print $2}')
-        mkdir -p ${workspace}/clusterNetwork/bls${i}
-        expect create_bls_key.exp ${workspace}/clusterNetwork/bls${i} ${KEYPASS}
-        vote_addr=0x$(cat ${workspace}/clusterNetwork/bls${i}/bls/keystore/*json | jq .pubkey | sed 's/"//g')
-        if [ ${standalone} = true ]; then
-            continue
-        fi
-    done
-}
-
-function generate_static_peers() {
-    tool=${workspace}/bin/bootnode
-    num=$1
-    target=$2
-    staticPeers=""
-    for ((i = 0; i < $num; i++)); do
-        if [ $i -eq $target ]; then
-            continue
-        fi
-
-        file=${workspace}/clusterNetwork/node${i}/geth/nodekey
-        if [ ! -f "$file" ]; then
-            $tool -genkey $file
-        fi
-        port=30311
-        domain="bsc-node-${i}.bsc.svc.cluster.local"
-        if [ ! -z "$staticPeers" ]; then
-            staticPeers+=","
-        fi
-        staticPeers+='"'"enode://$($tool -nodekey $file -writeaddress)@$domain:$port"'"'
-    done
-
-    echo $staticPeers
-}
-
 function prepare_config() {
     rm -f ${workspace}/genesis/validators.conf
 
-    for ((i = 0; i < ${size}; i++)); do
-        for f in ${workspace}/clusterNetwork/validator${i}/keystore/*; do
+    for ((i=0;i<${size};i++));do
+        for f in ${workspace}/.local/bsc/validator${i}/keystore/*;do
             cons_addr="0x$(cat ${f} | jq -r .address)"
         done
-
-        for f in ${workspace}/clusterNetwork/validator${i}_fee/keystore/*; do
+        
+        for f in ${workspace}/.local/bsc/validator${i}_fee/keystore/*;do
             fee_addr="0x$(cat ${f} | jq -r .address)"
         done
-
-        mkdir -p ${workspace}/clusterNetwork/node${i}
+        
+        mkdir -p ${workspace}/.local/bsc/clusterNetwork/node${i}
         bbcfee_addrs=${fee_addr}
         powers="0x000001d1a94a2000"
-        mv ${workspace}/clusterNetwork/bls${i}/bls ${workspace}/clusterNetwork/node${i}/ && rm -rf ${workspace}/clusterNetwork/bls${i}
-        vote_addr=0x$(cat ${workspace}/clusterNetwork/node${i}/bls/keystore/*json | jq .pubkey | sed 's/"//g')
-        echo "${cons_addr},${bbcfee_addrs},${fee_addr},${powers},${vote_addr}" >>${workspace}/genesis/validators.conf
+        if [ ${standalone} = false ]; then
+            bbcfee_addrs=`${workspace}/bin/tbnbcli staking side-top-validators ${size} --side-chain-id=${BSC_CHAIN_NAME} --node="${nodeurl}" --chain-id=${BC_CHAIN_ID} --trust-node --output=json| jq -r ".[${i}].distribution_addr" |xargs ${workspace}/bin/tool -network-type 0 -addr`
+            powers=`${workspace}/bin/tbnbcli staking side-top-validators ${size} --side-chain-id=${BSC_CHAIN_NAME} --node="${nodeurl}" --chain-id=${BC_CHAIN_ID} --trust-node --output=json| jq -r ".[${i}].tokens" |xargs ${workspace}/bin/tool -network-type 0 -power`
+        fi
+        mv ${workspace}/.local/bsc/bls${i}/bls ${workspace}/.local/bsc/clusterNetwork/node${i}/ && rm -rf ${workspace}/.local/bsc/bls${i}
+        vote_addr=0x$(cat ${workspace}/.local/bsc/clusterNetwork/node${i}/bls/keystore/*json| jq .pubkey | sed 's/"//g')
+        echo "${cons_addr},${bbcfee_addrs},${fee_addr},${powers},${vote_addr}" >> ${workspace}/genesis/validators.conf
         echo "validator" ${i} ":" ${cons_addr}
         echo "validatorFee" ${i} ":" ${fee_addr}
         echo "validatorVote" ${i} ":" ${vote_addr}
     done
 
     cd ${workspace}/genesis/
-    git stash push -- contracts/
-    sed -i -e "s/'0x' + publicKey.pop()/vs[4]/g" scripts/generate-validator.js
-    node scripts/generate-validator.js
-    node scripts/generate-initHolders.js --initHolders ${INIT_HOLDER}
+    node generate-validator.js
+    node generate-initHolders.js --initHolders ${INIT_HOLDER}
     if [ ${standalone} = false ]; then
-        initConsensusStateBytes=$(${workspace}/bin/tool -height 1 -rpc ${BC_NODE_URL} -network-type 0)
-        bash scripts/generate.sh local --chainId ${BSC_CHAIN_ID} --whitelist1Address ${INIT_HOLDER} --initConsensusStateBytes ${initConsensusStateBytes}
+        initConsensusStateBytes=$(${workspace}/bin/tool -height 1 -rpc ${nodeurl} -network-type 0)
+        node generate-genesis.js --chainid ${BSC_CHAIN_ID} --network 'local' --whitelist1Address ${INIT_HOLDER} --initConsensusStateBytes  ${initConsensusStateBytes}
     else
-        bash scripts/generate.sh local --chainId ${BSC_CHAIN_ID} --whitelist1Address ${INIT_HOLDER}
+        node generate-genesis.js --chainid ${BSC_CHAIN_ID} --network 'local' --whitelist1Address ${INIT_HOLDER}
     fi
+
+}
+function generate_static_peers() {
+    tool=${workspace}/bin/bootnode
+    num=$1
+    target=$2
+    staticPeers=""
+    for ((i=0;i<$num;i++)); do
+        if [ $i -eq $target ]
+        then
+           continue
+        fi
+
+        file=${workspace}/.local/bsc/clusterNetwork/node${i}/geth/nodekey
+        if [ ! -f "$file" ]; then
+            $tool -genkey $file
+        fi
+        port=30311
+        node_ip=${bc_node_ips[${i}]}
+        if [ ! -z "$staticPeers" ]
+        then
+            staticPeers+=","
+        fi
+        staticPeers+='"'"enode://$($tool -nodekey $file -writeaddress)@$node_ip:$port"'"'
+    done
+
+    echo $staticPeers
 }
 
 function initNetwork() {
     cd ${workspace}
-    ${workspace}/bin/geth init-network --init.dir ${workspace}/clusterNetwork --init.size=${size} --init.ips ${initIp} --config ${workspace}/config.toml ${workspace}/genesis/genesis.json
-    rm -rf ${workspace}/*bsc.log*
+    ${workspace}/bin/geth init-network --init.dir ${workspace}/.local/bsc/clusterNetwork --init.size=${size} --config ${workspace}/config.toml ${workspace}/genesis/genesis.json
+    rm -rf  ${workspace}/*bsc.log*
     for ((i = 0; i < ${size}; i++)); do
-        sed -i -e '/"<nil>"/d' ${workspace}/clusterNetwork/node${i}/config.toml
+        staticPeers=$(generate_static_peers ${size} ${i})
+        line=`grep -n -e 'StaticNodes' ${workspace}/.local/bsc/clusterNetwork/node${i}/config.toml | cut -d : -f 1`
+        head -n $((line-1)) ${workspace}/.local/bsc/clusterNetwork/node${i}/config.toml >> ${workspace}/.local/bsc/clusterNetwork/node${i}/config.toml-e
+        echo "StaticNodes = [${staticPeers}]" >> ${workspace}/.local/bsc/clusterNetwork/node${i}/config.toml-e
+        tail -n +$(($line+1)) ${workspace}/.local/bsc/clusterNetwork/node${i}/config.toml >> ${workspace}/.local/bsc/clusterNetwork/node${i}/config.toml-e
+        rm -f ${workspace}/.local/bsc/clusterNetwork/node${i}/config.toml
+        mv ${workspace}/.local/bsc/clusterNetwork/node${i}/config.toml-e ${workspace}/.local/bsc/clusterNetwork/node${i}/config.toml
 
-        cp -R ${workspace}/clusterNetwork/validator${i}/keystore ${workspace}/clusterNetwork/node${i}
-        for j in ${workspace}/clusterNetwork/validator${i}/keystore/*; do
+        sed -i -e '/"<nil>"/d' ${workspace}/.local/bsc/clusterNetwork/node${i}/config.toml
+
+        cp -R ${workspace}/.local/bsc/clusterNetwork/validator${i}/keystore ${workspace}/.local/bsc/clusterNetwork/node${i}
+        for j in ${workspace}/.local/bsc/clusterNetwork/validator${i}/keystore/*; do
             cons_addr="0x$(cat ${j} | jq -r .address)"
         done
 
-        cp ${workspace}/bin/geth ${workspace}/clusterNetwork/node${i}/geth${i}
+        cp ${workspace}/bin/geth ${workspace}/.local/bsc/clusterNetwork/node${i}/geth${i}
         # init genesis
-        ${workspace}/clusterNetwork/node${i}/geth${i} --datadir ${workspace}/clusterNetwork/node${i} init ${workspace}/genesis/genesis.json
+        ${workspace}/.local/bsc/clusterNetwork/node${i}/geth${i} --datadir ${workspace}/.local/bsc/clusterNetwork/node${i} init ${workspace}/genesis/genesis.json
     done
 }
 
 CMD=$1
 case ${CMD} in
-register)
-    echo "===== register ===="
+cluster_up)
+    echo "===== generate configs ===="
     register_validator
-    echo "===== end ===="
-    ;;
-generate)
-    echo "===== generate configs ===="
-    create_validator
-    prepare_config
-    initNetwork
-    echo "===== end ===="
-    ;;
-native_init)
-    echo "===== create_validator ===="
-    create_validator
-    echo "===== end ===="
-    echo "===== generate configs ===="
     prepare_config
     initNetwork
     echo "===== end ===="
     ;;
 *)
-    echo "Usage: setup_bsc_node.sh | register | generate | native_init"
+    echo "Usage: setup_bsc_node.sh cluster_up"
     ;;
 esac
