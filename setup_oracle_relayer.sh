5f9958d13a6d35b87919d68e584d3048f2686b12
j75689 <j75689@gmail.com>
2023-12-08 01:25:41 +0800

    fix: setup_oracle_relayer.sh

--- setup_oracle_relayer.sh
+++ setup_oracle_relayer.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 basedir=$(cd `dirname $0`; pwd)
 workspace=${basedir}
+bc_node_ips=(${BC_NODE_IP})
 dst_id="i-0d2b8632af953d0f6"
 
 function exit_previous() {
@@ -26,7 +27,7 @@ function init_config(){
     cp ${workspace}/oracle_relayer.template ${workspace}/.local/relayer/oracle_relayer.json
 
     sed -i -e "s/{{bsc_chain_id}}/${BSC_CHAIN_ID}/g" ${workspace}/.local/relayer/oracle_relayer.json
-    mnemonic="\"$(cat ${workspace}/.local/bc/node0/delegator.info |tail  -1)\""
+    mnemonic="\"$(cat ${workspace}/.local/bc/node0/operator.info |tail  -1)\""
     sed -i -e "s/{{bbc_mnemonic}}/${mnemonic}/g" ${workspace}/.local/relayer/oracle_relayer.json
 }
 
@@ -36,6 +37,15 @@ function prepare_native_config() {
 }
 
 function cluster_up() {
+    for ((i = 0; i < ${#bc_node_ips[@]}; i++)); do
+        operator=$(${workspace}/bin/tbnbcli keys list --home ${workspace}/.local/bc/node${i} | grep node${i} | awk '$1 == "node'${i}'" {print $3}')
+        echo "${KEYPASS}" | ${workspace}/bin/tbnbcli send --from node0-delegator --to $operator --amount 20000000000:BNB --chain-id ${BC_CHAIN_ID} --trust-node --node ${BC_NODE_URL} --home ${workspace}/.local/bc/node0
+        sleep 6 #wait for including tx in block
+
+        # vote
+        echo "${KEYPASS}" | ${workspace}/bin/tbnbcli gov vote --from node${i} --proposal-id ${proposal_id} --option Yes --chain-id ${BC_CHAIN_ID} --trust-node --node ${BC_NODE_URL} --home ${workspace}/.local/bc/node${i}
+    done
+
     cp ${workspace}/tmp/oracle-relayer/build/oracle-relayer ${workspace}/.local/relayer/
     rm -rf /mnt/efs/bsc-qa/bc-fusion/relayer
     mkdir -p /mnt/efs/bsc-qa/bc-fusion/relayer
