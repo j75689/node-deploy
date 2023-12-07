96bcbc4e56390d28f574479457430def01ba2a3f
j75689 <j75689@gmail.com>
2023-12-08 03:27:15 +0800

    fix: bsc setup

--- setup_bsc_node.sh
+++ setup_bsc_node.sh
@@ -90,9 +90,8 @@ function prepare_config() {
 
     cd ${workspace}/genesis/
     npm install
+    sed -i -e "s/address public constant WHITELIST_1 = 0xA904540818AC9c47f2321F97F1069B9d8746c6DB;/address public constant WHITELIST_1 = ${INIT_HOLDER};/g" ${workspace}/genesis/contracts/RelayerHub.template
     node generate-validator.js
-    sed -i -e "s/address public constant WHITELIST_1 = 0xb005741528b86F5952469d80A8614591E3c5B632;;/address public constant WHITELIST_1 = ${INIT_HOLDER};/g" ${workspace}/genesis/contracts/RelayerHub.template
-    npm run flatten
     if [ ${standalone} = false ]; then
         initConsensusStateBytes=$(${workspace}/bin/tool -height 1 -rpc ${nodeurl} -network-type 0)
         node generate-genesis.js --chainid 714 --bscChainId 02ca --network 'local' --initConsensusStateBytes  ${initConsensusStateBytes}
