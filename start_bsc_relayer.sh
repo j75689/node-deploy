5fea016df8dd593e5556f755cd749a3e25d1aa12
j75689 <j75689@gmail.com>
2023-12-08 01:19:31 +0800

    fix: setup relayer scripts

--- start_bsc_relayer.sh
+++ start_bsc_relayer.sh
@@ -7,6 +7,5 @@ yes | cp -rf /mnt/efs/bsc-qa/bc-fusion/relayer/* /server/relayer/
 if [ "${cmd}" == "reset" ]; then
     rm -rf /server/relayer/bsc_relayer.db
 fi
-rm -rf /server/relayer/bsc_relayer.db
 
-nohup /server/relayer/bsc-relayer --bbc-network-type 0 --config-type local --config-path /server/relayer/bsc_relayer.json > /server/relayer/bsc_relayer.log 2>&1 &
+sudo nohup /server/relayer/bsc-relayer --bbc-network-type 0 --config-type local --config-path /server/relayer/bsc_relayer.json > /server/relayer/bsc_relayer.log 2>&1 &
--- start_oracle_relayer.sh
+++ start_oracle_relayer.sh
@@ -7,6 +7,5 @@ yes | cp -rf /mnt/efs/bsc-qa/bc-fusion/relayer/* /server/relayer/
 if [ "${cmd}" == "reset" ]; then
     rm -rf /server/relayer/oracle_relayer.db
 fi
-rm -rf /server/relayer/oracle_relayer.db
 
-nohup /server/relayer/oracle-relayer --bbc-network 0 --config-type local --config-path /server/relayer/oracle_relayer.json > /server/relayer/oracle_relayer.log 2>&1 &
+sudo nohup /server/relayer/oracle-relayer --bbc-network 0 --config-type local --config-path /server/relayer/oracle_relayer.json > /server/relayer/oracle_relayer.log 2>&1 &
