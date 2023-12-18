05b6280dfc8c7b8faa9287a9a14969d63008a49d
j75689 <j75689@gmail.com>
2023-12-18 15:16:26 +0800

    fix: bsc setup

--- setup_bsc_node.sh
+++ setup_bsc_node.sh
@@ -102,6 +102,9 @@ function prepare_config() {
     yes | cp -f ${workspace}/init_holder.js ${workspace}/genesis/init_holders.js
     node generate-validator.js
     
+    # tmp fix for bsc-genesis
+    mv ${workspace}/genesis/scripts/generate-crossChain.sh mv ${workspace}/genesis/scripts/generate-crosschain.sh
+
     initConsensusStateBytes=$(${workspace}/bin/tool -height 1 -rpc ${nodeurl} -network-type 0)
     sed -i -e "s/42696e616e63652d436861696e2d4e696c650000000000000000000000000000000000000000000229eca254b3859bffefaf85f4c95da9fbd26527766b784272789c30ec56b380b6eb96442aaab207bc59978ba3dd477690f5c5872334fc39e627723daa97e441e88ba4515150ec3182bc82593df36f8abb25a619187fcfab7e552b94e64ed2deed000000e8d4a51000/${initConsensusStateBytes}/g" ${workspace}/genesis/scripts/generate.sh
     bash scripts/generate.sh local
