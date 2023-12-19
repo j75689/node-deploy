e122b9f526a58cb10b34ad8c10dc9375eb1c944a
j75689 <j75689@gmail.com>
2023-12-19 23:53:26 +0800

    fix: dump_bc sh

--- dump_bc_account.sh
+++ dump_bc_account.sh
@@ -5,4 +5,4 @@ rm -rf /server/bc/node-dump/output
 rm -rf /server/bc/node-dump/dump.log
 yes | cp -rf /mnt/efs/bsc-qa/bc-fusion/dump_bc_account/dump /server/bc/node-dump/dump
 
-/server/bc/node-dump/dump export /server/bc/node-dump/output --home /server/bnbchaind/disaster/node/gaiad > /server/bc/node-dump/dump.log 2>&1
+/server/bc/node-dump/dump export /server/bc/node-dump/output --home /server/bc/node > /server/bc/node-dump/dump.log 2>&1
