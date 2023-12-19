959e96d3c0e9f0e1daa76692f50f7f42402a0d3f
j75689 <j75689@gmail.com>
2023-12-20 00:00:47 +0800

    fix: missing folder for dumped output

--- dump_bc_account.sh
+++ dump_bc_account.sh
@@ -3,8 +3,10 @@
 mkdir -p /server/bc/node-dump
 rm -rf /server/bc/node-dump/output
 rm -rf /server/bc/node-dump/dump.log
+
 yes | cp -rf /mnt/efs/bsc-qa/bc-fusion/dump_bc_account/dump /server/bc/node-dump/dump
 
+mkdir -p /server/bc/node-dump/output
 /server/bc/node-dump/dump export /server/bc/node-dump/output --home /server/bc/node > /server/bc/node-dump/dump.log 2>&1
 
 rm -rf /mnt/efs/bsc-qa/bc-fusion/dump_bc_account/output
