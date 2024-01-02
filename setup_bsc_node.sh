796116dd5e98359dff1354c946de3aa8473cc249
j75689 <j75689@gmail.com>
2024-01-02 14:05:59 +0800

    fix: move node ids to ip2nodeids.sh

--- setup_bsc_node.sh
+++ setup_bsc_node.sh
@@ -6,15 +6,11 @@ basedir=$(
 workspace=${basedir}
 source ${workspace}/.env
 source ${workspace}/utils.sh
+source ${workspace}/ip2nodeids.sh
 size=$((${BSC_CLUSTER_SIZE}))
 standalone=false
 bsc_node_ips=(${BSC_NODE_IP})
 nodeurl=${BC_NODE_URL}
-
-declare -A ips2ids
-ips2ids["172.22.42.13"]="i-0d2b8632af953d0f6"
-ips2ids["172.22.42.94"]="i-001b988ca374e66f1"
-ips2ids["172.22.43.86"]="i-0d36ebf557138f8e5"
 
 # need a clean bc without stakings
 function register_validator() {
