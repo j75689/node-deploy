fc70a8f80001678fe9cd22f6dc235f9461d3d849
j75689 <j75689@gmail.com>
2024-01-23 11:21:56 +0800

    fix: approver env

--- dump_bc_account.sh
+++ dump_bc_account.sh
@@ -15,5 +15,5 @@ echo HTTP_PORT=8088 >> /server/token_approver.env
 echo LOGGER_LEVEL=DEBUG >> /server/token_approver.env
 echo LOGGER_FORMAT=json >> /server/token_approver.env
 echo SECRET_LOCAL_SECRET_PRIVATE_KEY=faec46db1c5b777dfce177421d639a432eef2644d1e79866136b4d80b1e41453 >> /server/token_approver.env
-echo STORE_MEMORY_STORE_ACCOUNTS=/server/token_approver/accounts.json >> /server/token_approver.env
-echo STORE_MEMORY_STORE_MERKLE_PROOFS=/server/token_approver/merkle_proofs.json >> /server/token_approver.env
+echo STORE_MEMORY_STORE_ACCOUNTS=/data/accounts.json >> /server/token_approver.env
+echo STORE_MEMORY_STORE_MERKLE_PROOFS=/data/merkle_proofs.json >> /server/token_approver.env
