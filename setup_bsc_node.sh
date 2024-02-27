5583cf5e7bd25e4a81bbc937dd2c9fb284f7bdbe
j75689 <j75689@gmail.com>
2024-02-27 18:48:42 +0800

    feat: set identity to new validator

--- setup_bsc_node.sh
+++ setup_bsc_node.sh
@@ -351,12 +351,15 @@ function migrate_validator() {
         operator_priv_file=${f}
     done
     operator_priv=$(${workspace}/bin/migrate_tool -secret ${operator_priv_file} -password ${KEYPASS})
-
+    identity=$(echo "${KEYPASS}" | ${workspace}/bin/tbnbcli validator-ownership sign-validator-ownership --bsc-operator-address ${operator_addr} --from node${validator_index}-delegator --chain-id "Binance-Chain-Ganges" --home ${workspace}/.local/bc/node${validator_index})
+    echo "identity:" ${identity}
+    exit 0
+    
     ${workspace}/bin/migrate_tool -priv_key ${operator_priv} -bsc_endpoint ${BSC_NODE_URL} \
      -delegation ${BSC_CREATE_DELEGATE_AMOUNT} \
      -consensus_addr ${cons_addr} -vote_addr ${vote_addr} -vote_bls_proof ${vote_addr_proof} \
      -commission_rate 800 -commission_max_rate 950 -commission_max_change_rate 300 \
-     -moniker "Nval${validator_index}" -details ${cons_addr} -identity ${operator_addr}
+     -moniker "Nval${validator_index}" -details ${cons_addr} -identity ${identity}
 
     while true; do
         if ${workspace}/bin/migrate_tool -get_validator_set -bsc_endpoint "${BSC_NODE_URL}" | grep -q "${cons_addr}"; then
